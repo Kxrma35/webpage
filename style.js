a15cb44c0ad1ce487e14e90e2d7b31dd290d8646
kxrma35 <karmanjeruh5@gmail.com>
2026-02-22 18:22:37 +0300

    add DOM Features

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-// Grab elements
+// DOM elements
 const userForm = document.getElementById("userForm");
 const nameInput = document.getElementById("name");
 const ageInput = document.getElementById("age");
