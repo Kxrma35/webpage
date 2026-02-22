f7d34aab2241c3be6a661ca9733f8dd6b6759cc4
kxrma35 <karmanjeruh5@gmail.com>
2026-02-22 16:31:42 +0300

    adding DOM Elements

---
+++ style.js
@@ -0,0 +1,11 @@
+// DOM elements
+const userForm = document.getElementById("userForm");
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+const content = document.getElementById("content");
+const greeting = document.getElementById("greeting");
+const adultMessage = document.getElementById("adultMessage");
+const ageInMonths = document.getElementById("ageInMonths");
+const quotesContainer = document.getElementById("quotes");
+const resetBtn = document.getElementById("resetBtn");
+const backToForm = document.getElementById("backToForm");
